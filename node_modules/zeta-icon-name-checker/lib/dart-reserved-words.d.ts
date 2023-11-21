45953a045cceb298a6f5cb92917c9773a1e507df
mikecoomber <mike.coomber@zebra.com>
2023-11-21 16:46:13 +0000

    removing node modules

--- node_modules/zeta-icon-name-checker/lib/dart-reserved-words.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const dartReservedWords: string[];
--- node_modules/zeta-icon-name-checker/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ZetaIconError, ErrorType, ErrorSeverity } from "./error.js";
-export { checkIconName, checkCategoryName, ZetaIconError, ErrorType, ErrorSeverity, };
-declare function checkIconName(iconName: string, categoryName?: string, usedNames?: string[]): ZetaIconError;
-declare function checkCategoryName(categoryName: string): ZetaIconError;

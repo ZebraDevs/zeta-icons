bc2b231aae594d5432e98b252afb21ae9f5a4693
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2023-11-27 23:01:50 +0000

    Version 0.1.37

--- build_files/icon-types.ts
+++ build_files/icon-types.ts
@@ -1,401 +1,404 @@
-export type ZetaIconName = 
-  | "alarm" 
-  | "bookmark_outline" 
-  | "build" 
-  | "cached" 
-  | "calendar_3_day" 
-  | "calendar_alt_1" 
-  | "calendar_alt_2" 
-  | "calendar_available" 
-  | "calendar_cancel" 
-  | "calendar_day" 
-  | "calendar_edit" 
-  | "calendar_monthly_alt" 
-  | "calendar_range" 
-  | "calendar_weekly_alt" 
-  | "calendar_yearly" 
-  | "check_circle_outline_alt" 
-  | "check_mark" 
-  | "clipboard" 
-  | "clock_outline" 
-  | "credit_card" 
-  | "delete_forever" 
-  | "delete_outline" 
-  | "dislike_action" 
-  | "document" 
-  | "download" 
-  | "filter_alt" 
-  | "hand" 
-  | "history" 
-  | "home" 
-  | "info" 
-  | "label" 
-  | "lock_alt" 
-  | "log_in" 
-  | "log_out" 
-  | "love_outline" 
-  | "open_in_full" 
-  | "open_in_new_window" 
-  | "pending" 
-  | "pin" 
-  | "price" 
-  | "renew" 
-  | "search" 
-  | "security_check" 
-  | "settings" 
-  | "sync_action" 
-  | "touch" 
-  | "trending_down" 
-  | "trending_up" 
-  | "unlock_alt" 
-  | "update" 
-  | "upload" 
-  | "user_circle" 
-  | "user_settings" 
-  | "verified" 
-  | "visibility_off" 
-  | "world_action" 
-  | "zoom_in" 
-  | "zoom_out" 
-  | "add_alert" 
-  | "auto_delete" 
-  | "error_outline" 
-  | "important_notification" 
-  | "warning_outline" 
-  | "block" 
-  | "closed_caption" 
-  | "fast_forward" 
-  | "fast_rewind" 
-  | "loop" 
-  | "microphone_off" 
-  | "microphone_outline" 
-  | "pause_circle" 
-  | "play_circle" 
-  | "play_outline" 
-  | "replay" 
-  | "skip_next" 
-  | "skip_previous" 
-  | "stop_circle" 
-  | "video_camera" 
-  | "volume_down" 
-  | "volume_mute" 
-  | "volume_off" 
-  | "volume_up" 
-  | "add_call" 
-  | "chat_bubble_outline" 
-  | "conversation" 
-  | "dialpad" 
-  | "do_not_disturb" 
-  | "dollar" 
-  | "email_alt" 
-  | "email_outline" 
-  | "end_call" 
-  | "message" 
-  | "person_search" 
-  | "phone_bluetooth_speaker" 
-  | "phone_in_talk" 
-  | "priority" 
-  | "qr_code" 
-  | "qr_code_scan" 
-  | "rss_feed" 
-  | "sd_card" 
-  | "sim_card" 
-  | "sms" 
-  | "sync_disabled" 
-  | "add_box" 
-  | "add_circle_outline" 
-  | "analysis" 
-  | "android" 
-  | "backspace" 
-  | "block_content" 
-  | "clear" 
-  | "copy_file" 
-  | "create" 
-  | "filter_list" 
-  | "flag" 
-  | "link_content" 
-  | "push_pin" 
-  | "redo" 
-  | "remove_box" 
-  | "remove_circle_outline" 
-  | "reply" 
-  | "save_alt" 
-  | "send" 
-  | "sort" 
-  | "undo" 
-  | "antenna" 
-  | "battery_alert" 
-  | "battery_charging" 
-  | "bluetooth_disabled" 
-  | "bluetooth_searching" 
-  | "brightness" 
-  | "cellular_signal" 
-  | "chain" 
-  | "contrast" 
-  | "dark_mode" 
-  | "devices" 
-  | "ethernet" 
-  | "flight_mode" 
-  | "hdmi" 
-  | "location_point" 
-  | "mobile_friendly" 
-  | "network_signal" 
-  | "nfc" 
-  | "night" 
-  | "restart_alt" 
-  | "screen_rotation" 
-  | "uhf_rfid" 
-  | "usb_alt_device" 
-  | "volte" 
-  | "appgallery" 
-  | "application_analytics" 
-  | "asset_tracker_lite" 
-  | "blood_bag_plus" 
-  | "bluetooth_management" 
-  | "browser_print" 
-  | "cloud_connect" 
-  | "data_wedge" 
-  | "design_tools" 
-  | "device_tracker" 
-  | "direct_connect" 
-  | "emdk" 
-  | "enterprise_browser" 
-  | "enterprise_connectors" 
-  | "enterprise_keyboard" 
-  | "gms_restricted_mode" 
-  | "intelligent_document_capture" 
-  | "label_plus" 
-  | "life_guard" 
-  | "link_os" 
-  | "mdm_connectors" 
-  | "mdm_toolkit" 
-  | "mobility_dna" 
-  | "mobility_security" 
-  | "multi_code_data_formatting" 
-  | "mx" 
-  | "ocr" 
-  | "oem_config" 
-  | "pairing_solutions" 
-  | "pdf_direct" 
-  | "power_precision" 
-  | "preferred_symbol" 
-  | "print_secure" 
-  | "print_station" 
-  | "printer_profile_manager" 
-  | "przm" 
-  | "remote_control" 
-  | "remote_diagnostics" 
-  | "remote_management" 
-  | "rx" 
-  | "scan_and_pair" 
-  | "scan_speed_analytics" 
-  | "scan_to_connect" 
-  | "scanner_control_application" 
-  | "setting_tool" 
-  | "simulscan" 
-  | "smart_te_lite" 
-  | "smart_tek" 
-  | "smartdex" 
-  | "stagenow" 
-  | "swipe_assist" 
-  | "tekspeech_pro" 
-  | "virtual_devices" 
-  | "virtual_tether" 
-  | "visibility_services" 
-  | "voice_wedge" 
-  | "workforce_connect" 
-  | "workstation_connect" 
-  | "zebra_designer" 
-  | "zebra_onecare" 
-  | "zebra_setup_utility" 
-  | "zero_touch" 
-  | "align_center" 
-  | "align_horizontal_center" 
-  | "align_horizontal_left" 
-  | "align_horizontal_right" 
-  | "align_left" 
-  | "align_right" 
-  | "align_vertical_bottom" 
-  | "align_vertical_center" 
-  | "align_vertical_top" 
-  | "attach" 
-  | "bold" 
-  | "bullet_list" 
-  | "checklist" 
-  | "color_fill" 
-  | "distribute_horizontal" 
-  | "distribute_vertical" 
-  | "edit_border" 
-  | "font_size_decrease" 
-  | "font_size_increase" 
-  | "format_shapes" 
-  | "indent_decrease" 
-  | "indent_increase" 
-  | "italic" 
-  | "justify" 
-  | "line_spacing" 
-  | "no_color" 
-  | "numbered_list" 
-  | "strikethrough" 
-  | "text_color" 
-  | "underline" 
-  | "barcode_bluetooth" 
-  | "barcode_done" 
-  | "barcode_image_scan" 
-  | "barcode_qr_code" 
-  | "barcode_scanner_down" 
-  | "barcode_settings" 
-  | "calculator" 
-  | "card_orientation" 
-  | "card_with_chip" 
-  | "certificate" 
-  | "choices" 
-  | "contactless_pay" 
-  | "credit_card_swipe" 
-  | "enterprise_diagnostics" 
-  | "environmental" 
-  | "factory" 
-  | "freezer" 
-  | "issue_audit" 
-  | "performance" 
-  | "planogram" 
-  | "productivity_apps" 
-  | "receipt" 
-  | "retail" 
-  | "road_map" 
-  | "rules" 
-  | "scan_rate" 
-  | "shape_recognition" 
-  | "steps" 
-  | "strategy" 
-  | "tap_to_pay" 
-  | "zero_scale" 
-  | "attachment" 
-  | "cloud_done" 
-  | "cloud_download" 
-  | "cloud_off" 
-  | "cloud_outline" 
-  | "cloud_upload" 
-  | "create_new_folder" 
-  | "doc_attach" 
-  | "downloading" 
-  | "file_download" 
-  | "file_upload" 
-  | "finished_download" 
-  | "folder_outline" 
-  | "grid_view" 
-  | "jpg_attach" 
-  | "pdf_attach" 
-  | "png_attach" 
-  | "ppt_attach" 
-  | "shared_folder" 
-  | "upload_file" 
-  | "cast_connected" 
-  | "desktop" 
-  | "device_settings" 
-  | "devices_ecosystem" 
-  | "headphones" 
-  | "laptop" 
-  | "memory" 
-  | "monitor" 
-  | "phone_android" 
-  | "phone_iphone" 
-  | "phonelink" 
-  | "smart_phone" 
-  | "tablet_mac" 
-  | "tv" 
-  | "video_play" 
-  | "watch" 
-  | "adjustments" 
-  | "awb" 
-  | "camera_shutter" 
-  | "contrast_image" 
-  | "eye_dropper" 
-  | "flip_camera" 
-  | "grid_off" 
-  | "grid_on" 
-  | "hdr" 
-  | "image_library" 
-  | "iso" 
-  | "portrait" 
-  | "rotate_left" 
-  | "rotate_right" 
-  | "slide_show" 
-  | "straighten" 
-  | "timer" 
-  | "visibility_image" 
-  | "visibility_off_image" 
-  | "wb_image" 
-  | "bus" 
-  | "cafe" 
-  | "layers" 
-  | "light_rail_train" 
-  | "local_shipping" 
-  | "my_location" 
-  | "navigation" 
-  | "near_me" 
-  | "offer" 
-  | "place" 
-  | "taxi" 
-  | "zoom_in_map" 
-  | "zoom_out_map" 
-  | "arrow_back" 
-  | "arrow_down" 
-  | "arrow_forward" 
-  | "arrow_up" 
-  | "cancel_outline" 
-  | "caret_down" 
-  | "caret_left" 
-  | "caret_right" 
-  | "caret_sort" 
-  | "caret_up" 
-  | "chevron_left" 
-  | "chevron_right" 
-  | "columns" 
-  | "dashboard" 
-  | "drop_down_circle" 
-  | "expand_less" 
-  | "expand_more" 
-  | "first_page" 
-  | "full_screen_exit" 
-  | "hamburger_menu_navigation_rail" 
-  | "hamburger_menu_thick" 
-  | "help_outline" 
-  | "last_page" 
-  | "list_alt" 
-  | "more_horizontal" 
-  | "more_vertical" 
-  | "refresh" 
-  | "unfold_less" 
-  | "unfold_more" 
-  | "add_group" 
-  | "add_person" 
-  | "bad_mood" 
-  | "blog" 
-  | "facebook" 
-  | "groups" 
-  | "happy" 
-  | "instagram" 
-  | "ios_share" 
-  | "linkedin" 
-  | "medium" 
-  | "notifications_active" 
-  | "notifications_outline" 
-  | "person_outline" 
-  | "remove_group" 
-  | "remove_person" 
-  | "sad" 
-  | "satisfied" 
-  | "switch_user" 
-  | "twitter" 
-  | "very_satisfied" 
-  | "world_map" 
-  | "youtube" 
-  | "check_box_empty" 
-  | "indeterminate_check_box" 
-  | "radio_button_checked" 
-  | "radio_button_unchecked" 
-  | "star_half" 
-  | "star_outline" 
-  | "toggle_off" 
-  | "toggle_on"; 
+export const IconNameList = [ 
+"alarm",
+"bookmark_outline",
+"build",
+"cached",
+"calendar_3_day",
+"calendar_alt_1",
+"calendar_alt_2",
+"calendar_available",
+"calendar_cancel",
+"calendar_day",
+"calendar_edit",
+"calendar_monthly_alt",
+"calendar_range",
+"calendar_weekly_alt",
+"calendar_yearly",
+"check_circle_outline_alt",
+"check_mark",
+"clipboard",
+"clock_outline",
+"credit_card",
+"delete_forever",
+"delete_outline",
+"dislike_action",
+"document",
+"download",
+"filter_alt",
+"hand",
+"history",
+"home",
+"info",
+"label",
+"lock_alt",
+"log_in",
+"log_out",
+"love_outline",
+"open_in_full",
+"open_in_new_window",
+"pending",
+"pin",
+"price",
+"renew",
+"search",
+"security_check",
+"settings",
+"sync_action",
+"touch",
+"trending_down",
+"trending_up",
+"unlock_alt",
+"update",
+"upload",
+"user_circle",
+"user_settings",
+"verified",
+"visibility_off",
+"world_action",
+"zoom_in",
+"zoom_out",
+"add_alert",
+"auto_delete",
+"error_outline",
+"important_notification",
+"warning_outline",
+"block",
+"closed_caption",
+"fast_forward",
+"fast_rewind",
+"loop",
+"microphone_off",
+"microphone_outline",
+"pause_circle",
+"play_circle",
+"play_outline",
+"replay",
+"skip_next",
+"skip_previous",
+"stop_circle",
+"video_camera",
+"volume_down",
+"volume_mute",
+"volume_off",
+"volume_up",
+"add_call",
+"chat_bubble_outline",
+"conversation",
+"dialpad",
+"do_not_disturb",
+"dollar",
+"email_alt",
+"email_outline",
+"end_call",
+"message",
+"person_search",
+"phone_bluetooth_speaker",
+"phone_in_talk",
+"priority",
+"qr_code",
+"qr_code_scan",
+"rss_feed",
+"sd_card",
+"sim_card",
+"sms",
+"sync_disabled",
+"add_box",
+"add_circle_outline",
+"analysis",
+"android",
+"backspace",
+"block_content",
+"clear",
+"copy_file",
+"create",
+"filter_list",
+"flag",
+"link_content",
+"push_pin",
+"redo",
+"remove_box",
+"remove_circle_outline",
+"reply",
+"save_alt",
+"send",
+"sort",
+"undo",
+"antenna",
+"battery_alert",
+"battery_charging",
+"bluetooth_disabled",
+"bluetooth_searching",
+"brightness",
+"cellular_signal",
+"chain",
+"contrast",
+"dark_mode",
+"devices",
+"ethernet",
+"flight_mode",
+"hdmi",
+"location_point",
+"mobile_friendly",
+"network_signal",
+"nfc",
+"night",
+"restart_alt",
+"screen_rotation",
+"uhf_rfid",
+"usb_alt_device",
+"volte",
+"appgallery",
+"application_analytics",
+"asset_tracker_lite",
+"blood_bag_plus",
+"bluetooth_management",
+"browser_print",
+"cloud_connect",
+"data_wedge",
+"design_tools",
+"device_tracker",
+"direct_connect",
+"emdk",
+"enterprise_browser",
+"enterprise_connectors",
+"enterprise_keyboard",
+"gms_restricted_mode",
+"intelligent_document_capture",
+"label_plus",
+"life_guard",
+"link_os",
+"mdm_connectors",
+"mdm_toolkit",
+"mobility_dna",
+"mobility_security",
+"multi_code_data_formatting",
+"mx",
+"ocr",
+"oem_config",
+"pairing_solutions",
+"pdf_direct",
+"power_precision",
+"preferred_symbol",
+"print_secure",
+"print_station",
+"printer_profile_manager",
+"przm",
+"remote_control",
+"remote_diagnostics",
+"remote_management",
+"rx",
+"scan_and_pair",
+"scan_speed_analytics",
+"scan_to_connect",
+"scanner_control_application",
+"setting_tool",
+"simulscan",
+"smart_te_lite",
+"smart_tek",
+"smartdex",
+"stagenow",
+"swipe_assist",
+"tekspeech_pro",
+"virtual_devices",
+"virtual_tether",
+"visibility_services",
+"voice_wedge",
+"workforce_connect",
+"workstation_connect",
+"zebra_designer",
+"zebra_onecare",
+"zebra_setup_utility",
+"zero_touch",
+"align_center",
+"align_horizontal_center",
+"align_horizontal_left",
+"align_horizontal_right",
+"align_left",
+"align_right",
+"align_vertical_bottom",
+"align_vertical_center",
+"align_vertical_top",
+"attach",
+"bold",
+"bullet_list",
+"checklist",
+"color_fill",
+"distribute_horizontal",
+"distribute_vertical",
+"edit_border",
+"font_size_decrease",
+"font_size_increase",
+"format_shapes",
+"indent_decrease",
+"indent_increase",
+"italic",
+"justify",
+"line_spacing",
+"no_color",
+"numbered_list",
+"strikethrough",
+"text_color",
+"type",
+"underline",
+"barcode_bluetooth",
+"barcode_done",
+"barcode_image_scan",
+"barcode_qr_code",
+"barcode_scanner_down",
+"barcode_settings",
+"calculator",
+"card_orientation",
+"card_with_chip",
+"certificate",
+"choices",
+"contactless_pay",
+"credit_card_swipe",
+"enterprise_diagnostics",
+"environmental",
+"factory",
+"freezer",
+"issue_audit",
+"performance",
+"planogram",
+"productivity_apps",
+"receipt",
+"retail",
+"road_map",
+"rules",
+"scan_rate",
+"shape_recognition",
+"steps",
+"strategy",
+"tap_to_pay",
+"zero_scale",
+"attachment",
+"cloud_done",
+"cloud_download",
+"cloud_off",
+"cloud_outline",
+"cloud_upload",
+"create_new_folder",
+"doc_attach",
+"downloading",
+"file_download",
+"file_upload",
+"finished_download",
+"folder_outline",
+"grid_view",
+"jpg_attach",
+"pdf_attach",
+"png_attach",
+"ppt_attach",
+"shared_folder",
+"upload_file",
+"cast_connected",
+"desktop",
+"device_settings",
+"devices_ecosystem",
+"headphones",
+"laptop",
+"memory",
+"monitor",
+"phone_android",
+"phone_iphone",
+"phonelink",
+"smart_phone",
+"tablet_mac",
+"tv",
+"video_play",
+"watch",
+"adjustments",
+"awb",
+"camera_shutter",
+"contrast_image",
+"eye_dropper",
+"flip_camera",
+"grid_off",
+"grid_on",
+"hdr",
+"image_library",
+"iso",
+"portrait",
+"rotate_left",
+"rotate_right",
+"slide_show",
+"straighten",
+"timer",
+"visibility_image",
+"visibility_off_image",
+"wb_image",
+"bus",
+"cafe",
+"layers",
+"light_rail_train",
+"local_shipping",
+"my_location",
+"navigation",
+"near_me",
+"offer",
+"place",
+"taxi",
+"zoom_in_map",
+"zoom_out_map",
+"arrow_back",
+"arrow_down",
+"arrow_forward",
+"arrow_up",
+"cancel_outline",
+"caret_down",
+"caret_left",
+"caret_right",
+"caret_sort",
+"caret_up",
+"chevron_left",
+"chevron_right",
+"columns",
+"dashboard",
+"drop_down_circle",
+"expand_less",
+"expand_more",
+"first_page",
+"full_screen_exit",
+"hamburger_menu_navigation_rail",
+"hamburger_menu_thick",
+"help_outline",
+"last_page",
+"list_alt",
+"more_horizontal",
+"more_vertical",
+"refresh",
+"unfold_less",
+"unfold_more",
+"add_group",
+"add_person",
+"bad_mood",
+"blog",
+"facebook",
+"groups",
+"happy",
+"instagram",
+"ios_share",
+"linkedin",
+"medium",
+"notifications_active",
+"notifications_outline",
+"person_outline",
+"remove_group",
+"remove_person",
+"sad",
+"satisfied",
+"switch_user",
+"twitter",
+"very_satisfied",
+"world_map",
+"youtube",
+"check_box_empty",
+"indeterminate_check_box",
+"radio_button_checked",
+"radio_button_unchecked",
+"star_half",
+"star_outline",
+"toggle_off",
+"toggle_on",];
+ type IconNameTuple = typeof IconNameList;
+ export type IconNames = IconNameTuple[number];

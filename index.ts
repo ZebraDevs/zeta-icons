3c0ea54ff1508f7fbd4f62582a281d47b2276ac8
Luke <lwalton@zebra.com>
2024-04-22 17:21:24 +0100

    fix js import

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import iconManifest from "./outputs/icon-manifest.json";
-export { ZetaIconNameList } from "./outputs/definitions/icon-types";
+export { ZetaIconNameList } from "./outputs/definitions/icon-types.js";
 export { iconManifest };
 
-export type { ZetaIconName } from "./outputs/definitions/icon-types";
+export type { ZetaIconName } from "./outputs/definitions/icon-types.js";

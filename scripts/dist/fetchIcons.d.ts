8d80124f830dce3e44d65cd9ea74c2c3bb7878b6
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2024-04-22 14:44:32 +0000

    Version 0.2.1

--- scripts/dist/fetchIcons.d.ts
+++ scripts/dist/fetchIcons.d.ts
@@ -3,8 +3,11 @@
  *
  * @param {string} figmaToken - Figma authentication token. See {@link https://www.figma.com/developers/api#authentication | Figma API documentation}.
  * @param {string} figmaFileId - Figma file ID to retrieve icons from.
+ * @param {string} iconPageName - The name of the icon page from the given Figma file that the icons will be extracted from.
  * @param {string} oldHash - Hash of icons from cache.
  * @param {string} outputDir - Directory to (potentially) save icons to.
+ * @param {boolean} verboseLogs - Logs more verbose outputs for testing.
+ *
  * @returns {string | undefined} Hash of updated icons. If undefined, icons have not changed.
  */
 export default function main(figmaToken: string, figmaFileId: string, iconPageName: string, oldHash: string, outputDir: string, verboseLogs: boolean): Promise<string | undefined>;
--- scripts/dist/fetchIcons.js
+++ scripts/dist/fetchIcons.js
@@ -16,8 +16,11 @@ const tempDir = "/temp";
  *
  * @param {string} figmaToken - Figma authentication token. See {@link https://www.figma.com/developers/api#authentication | Figma API documentation}.
  * @param {string} figmaFileId - Figma file ID to retrieve icons from.
+ * @param {string} iconPageName - The name of the icon page from the given Figma file that the icons will be extracted from.
  * @param {string} oldHash - Hash of icons from cache.
  * @param {string} outputDir - Directory to (potentially) save icons to.
+ * @param {boolean} verboseLogs - Logs more verbose outputs for testing.
+ *
  * @returns {string | undefined} Hash of updated icons. If undefined, icons have not changed.
  */
 export default async function main(figmaToken, figmaFileId, iconPageName, oldHash, outputDir, verboseLogs) {

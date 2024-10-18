abe548dc2e8bab0b31dabeac2973a52ac01c61c4
mikecoomber <mike.coomber@zebra.com>
2024-10-18 14:13:31 +0100

    fix: Fixed code connect script

--- .github/create_code_connect/index.js
+++ .github/create_code_connect/index.js
@@ -2,8 +2,8 @@ import { readFileSync } from "fs";
 import { generateCodeConnectFile } from "../../dist/scripts/code-connect/generate-code-connect-file.js";
 
 const main = async () => {
-  const iconManifest = new Map(Object.entries(JSON.parse(readFileSync("../../outputs/icon-manifest.json"))));
-  const dir = "../../outputs";
+  const iconManifest = new Map(Object.entries(JSON.parse(readFileSync("outputs/icon-manifest.json"))));
+  const dir = "outputs";
 
   generateCodeConnectFile(dir, iconManifest);
 };

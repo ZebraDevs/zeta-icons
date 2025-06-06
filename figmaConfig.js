50493d63dc427dc7eb8c6ce2ba15a7dd7d6dde64
thelukewalton <lwalton@zebra.com>
2025-06-06 09:45:53 +0100

    chore: Update icon page name to match figma

--- figmaConfig.js
+++ figmaConfig.js
@@ -1,2 +1,2 @@
 export const ZDS_ASSETS_FILE_ID = "VQ7Aa3rDYB7mgpToI3bZ4D";
-export const ZDS_ASSETS_ICON_PAGE_NAME = "🦓 Icons - Parent";
+export const ZDS_ASSETS_ICON_PAGE_NAME = "🦓 All Zebra Icons - Parent";

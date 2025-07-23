1f0285fde5ee237f0a3cd31555aaf0d15790d2a6
Luke Walton <thelukewalton@users.noreply.github.com>
2025-07-23 14:55:29 +0100

    fix: Replace all hex colors with 000000 (#117)
    This is to prevent unnecessary updates when the colors are changed in figma

--- scripts/utils/api.ts
+++ scripts/utils/api.ts
@@ -1,6 +1,9 @@
 import { ImageManifest, IconManifest } from "../../types.js";
 import { DocumentResponse } from "../types/figmaTypes.js";
 
+const COLOR_REGEX = /#[0-9A-Fa-f]{6}(?<!000000)/g;
+const COLOR_REPLACE_BLACK = "#000000";
+
 /**
  * Fetches a Figma document.
  *
@@ -69,7 +72,11 @@ export async function getImageFiles(
   } = Object.fromEntries(new Map<string, string>(svgTextResponse));
 
   for (const [iconId, icon] of manifest) {
-    allImageFiles.set(iconId, { ...icon, roundData: svgObj[icon.roundId], sharpData: svgObj[icon.sharpId] });
+    allImageFiles.set(iconId, {
+      ...icon,
+      roundData: svgObj[icon.roundId].replace(COLOR_REGEX, COLOR_REPLACE_BLACK) || "",
+      sharpData: svgObj[icon.sharpId].replace(COLOR_REGEX, COLOR_REPLACE_BLACK) || "",
+    });
   }
 
   return allImageFiles;

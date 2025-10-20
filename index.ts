1c0d428233b06dc9ec45fa8fabcf438257eca99b
thelukewalton <lwalton@zebra.com>
2025-10-20 10:07:43 +0100

    deps: Update node deps [released]

--- index.ts
+++ index.ts
@@ -10,5 +10,5 @@ export type { ZetaIconName } from "./outputs/web/icon-types.js";
 export type * from "./types.js";
 
 // x-release-please-start-version
-export const version = "1.3.0";
+export const version = "1.4.0";
 // x-release-please-end

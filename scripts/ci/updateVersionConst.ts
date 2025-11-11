8914c2ec1d88870db3f6269fe49b8ec08233ded3
thelukewalton <lwalton@zebra.com>
2025-11-11 10:01:29 +0530

    fix: Dart version;

--- scripts/ci/updateVersionConst.ts
+++ scripts/ci/updateVersionConst.ts
@@ -1,12 +1,26 @@
 import { readFileSync, writeFileSync } from "fs";
 import packageJson from "../../package.json" with { type: "json" };
 
+const indexTsPath = "./index.ts";
+const dartTemplatePath = "./scripts/fetch-icons/templates/icons.dart.template";
+const dartOutputPath = "./outputs/flutter/icons.g.dart";
+
 const updateVersionConst = (): void => {
-  const indexTsPath = "./index.ts";
   let indexTsContent = readFileSync(indexTsPath).toString();
   const versionRegex = /const\s+version\s*=\s*['"`][^'"`]+['"`]/;
   const newVersionLine = `const version = '${packageJson.version}'`;
   indexTsContent = indexTsContent.replace(versionRegex, newVersionLine);
+
+  let dartTemplateContent = readFileSync(dartTemplatePath).toString();
+  const dartVersionRegex = /const\s+zetaIconsVersion\s*=\s*['"`][^'"`]+['"`];/;
+  const newDartVersionLine = `const zetaIconsVersion = '${packageJson.version}';`;
+
+  dartTemplateContent = dartTemplateContent.replace(dartVersionRegex, newDartVersionLine);
+  writeFileSync(dartTemplatePath, dartTemplateContent);
+
+  let dartGeneratedContent = readFileSync(dartOutputPath).toString();
+  dartGeneratedContent = dartGeneratedContent.replace(dartVersionRegex, newDartVersionLine);
+  writeFileSync(dartOutputPath, dartGeneratedContent);
 
   console.log(`Updating version in index.ts to ${packageJson.version}`);
   writeFileSync(indexTsPath, indexTsContent);
